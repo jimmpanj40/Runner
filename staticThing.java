2034e1ae06ca04c77be41a8d2c6f4ca23847a842
jimmpanj40 <93115336+jimmpanj40@users.noreply.github.com>
2021-11-02 19:39:35 +0100

    Update staticThing.java
    add setter

--- staticThing.java
+++ staticThing.java
@@ -1,26 +1,23 @@
-
-
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 public class staticThing {
-
     private double x;
     private double y;
     private ImageView imageview;
 
     public staticThing(double x, double y, String fileName){
-        Image image = new Image("/Images/"+fileName+".png");
-        this.imageview = new ImageView(image);
-        this.imageview.setX(x);
-        this.imageview.setY(y);
-
+        Image image = new Image("/Images/"+fileName+".png"); // Image récupère l'image d'adresse : "/Images/"+fileName+".png"
+        this.imageview = new ImageView(image); // On crée une image à voir
+        this.imageview.setX(x); // On attribue x à imageview
+        this.imageview.setY(y); // y
         this.x=x;
         this.y=y;
-
     }
-    public ImageView getImageView(){
-        return imageview;
-    }
-
+    
+// getters
+    public ImageView getImageView(){return imageview;}
+    public double getX() {return this.x;}
+// setter
+    public void setX(double x) {this.x=x;}
 }
