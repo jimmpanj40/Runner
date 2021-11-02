bdde973b1fda32d1be294516004330d33478d214
jimmpanj40 <93115336+jimmpanj40@users.noreply.github.com>
2021-11-02 19:38:10 +0100

    Update AnimatedThing.java
    add update

--- AnimatedThing.java
+++ AnimatedThing.java
@@ -1,3 +1,4 @@
+import javafx.geometry.Rectangle2D;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
@@ -8,40 +9,41 @@ public abstract class AnimatedThing {
     public ImageView spriteSheet;
     private int attitude;
 
-    private int index;
+    private int index=0;
     private double duration = 30;
     private int indexMax = 5;
     private double l;
     private double h;
     private double offset = 30;
 
+    //constructeur
     public AnimatedThing(double x, double y, String filename) {
-        Image thisSpringSheet = new Image("/Images/"+filename+".png");
-        this.spriteSheet=new ImageView(thisSpringSheet);
+        Image thisSpringSheet = new Image("/Images/"+filename+".png"); // Attribue à Image l'image animée (notre Hero)
+        this.spriteSheet=new ImageView(thisSpringSheet); // création de l'image à voir
         this.spriteSheet.setX(x);
         this.spriteSheet.setY(y);
+        spriteSheet.setViewport( new Rectangle2D(0,0,85,100)); // Rectangle2D crée un cadre 2D sur spritesheet, partant de (v,v1) et longueur v3 et de hauteur v4
         this.x=x;
         this.y=y;
-        this.setIndex(0);
-
-
     }
-
+    //Méthode
+    public void update(long time){ // permet d'actualiser l'image animée
+        int newindex = getIndex(); //Index permet de pointer les 6 positions différentes de notre Hero
+        if (newindex==5){setIndex(0);}
+        else{setIndex(newindex+1);}
+        x= getX();
+        //System.out.println(i);
+        spriteSheet.setViewport( new Rectangle2D(this.index*85,0,85,100)); // On affiche la nouvelle position du Hero à chaque appel de update
+    }
     //Setter
     public void setX(double x) {this.x = x;}
     public void setY(double y) {this.y = y;}
     public void setIndex(int index) {this.index = index;}
 
-    //constructeur
-
-
     //Getter
-   
     public double getX() {return x;}
-
     public double getY() {return y;}
-
-    public double getIndex() {return index;}
-
+    public int getIndex() {return index;}
     public abstract ImageView getSpriteSheet();
 }
+
