f4499c56590ac992e5e381f8d5978a388ba6a446
jimmpanj40 <93115336+jimmpanj40@users.noreply.github.com>
2021-11-02 19:42:49 +0100

    Update Main.java
    modification du cadre d'affichage

--- Main.java
+++ Main.java
@@ -6,21 +6,20 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        primaryStage.setTitle("Hello world");
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Run the world");
         Group root = new Group();
         Pane pane = new Pane(root);
-        GameScene theScene = new GameScene(pane, 1600, 400,true);
+        GameScene theScene = new GameScene(pane, 600, 400, true); //La partie qui nous intéresse. Sinon 1600 400 peret d'avoir les deux backGround
 
-        pane.getChildren().add(theScene.getRightBack().getImageView());
-        pane.getChildren().add(theScene.getLeftBack().getImageView());
-        pane.getChildren().add(theScene.getHero().getSpriteSheet());
-
-
-
-        primaryStage.setScene(theScene);
-        primaryStage.show();
+        pane.getChildren().add(theScene.getRightBack().getImageView()); // affichage du background gauche
+        pane.getChildren().add(theScene.getLeftBack().getImageView()); // " " droite
+        pane.getChildren().add(theScene.getHero().getSpriteSheet());// " " Hero
+        
+        primaryStage.setScene(theScene); //initialiser la scène
+        primaryStage.show(); // show
     }
+
     public static void main(String[] args) {
         launch(args);
     }
