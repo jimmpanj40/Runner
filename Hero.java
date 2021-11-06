5eed03a413acc15df241063e616ad1c699286fb6
jimmpanj40 <93115336+jimmpanj40@users.noreply.github.com>
2021-11-06 18:57:54 +0100

    Update Hero.java
    différentiation du spritesheet hero

--- Hero.java
+++ Hero.java
@@ -1,16 +1,16 @@
 import javafx.geometry.Rectangle2D;
 import javafx.scene.image.ImageView;
 
-public class Hero extends AnimatedThing {
+public class Hero extends AnimatedThing { // extends permet à Hero d'hériter de toutes les caractéristiques de AnimatedThing
 
     public Hero() {
         super(20, 245, "heros");
         this.getSpriteSheet().setViewport(new Rectangle2D(0,0,75,100));
     }
 
-
+//abstract méthode
     @Override
     public ImageView getSpriteSheet() {
-        return this.spriteSheet;
+        return this.spriteSheet_hero;
     }
 }
