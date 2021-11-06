327b7e07910d2dabc1ccf35ab5b6ab2ca0e5246b
jimmpanj40 <93115336+jimmpanj40@users.noreply.github.com>
2021-11-06 18:59:49 +0100

    Update Main.java
    add jump and foes view

--- Main.java
+++ Main.java
@@ -1,6 +1,9 @@
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
@@ -15,9 +18,22 @@ public class Main extends Application {
         pane.getChildren().add(theScene.getRightBack().getImageView()); // affichage du background gauche
         pane.getChildren().add(theScene.getLeftBack().getImageView()); // " " droite
         pane.getChildren().add(theScene.getHero().getSpriteSheet());// " " Hero
-        
+        pane.getChildren().add(theScene.getFoe().getSpriteSheet());// " " Foe
+
+
         primaryStage.setScene(theScene); //initialiser la scène
         primaryStage.show(); // show
+
+        theScene.setOnKeyPressed(new EventHandler<KeyEvent>() { //Code permettant de récupérer l'information d'un appuie de touche
+            @Override
+            public void handle(KeyEvent ke) {
+                if (ke.getCode() == KeyCode.SPACE) { // Si appuie de la touche SPACE
+                    Hero.jumpOk=1; //Permet de sauter
+                }
+            }
+        });
+
+
     }
 
     public static void main(String[] args) {
